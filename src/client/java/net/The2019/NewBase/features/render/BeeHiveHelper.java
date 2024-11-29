7ce93894357fd0b0aa10ca6f17b9e0b2def1e76d
The2019 <90553716+The2019@users.noreply.github.com>
2024-11-29 21:19:01 +0100

    changed renderings

--- src/client/java/net/The2019/NewBase/features/render/BeeHiveHelper.java
+++ src/client/java/net/The2019/NewBase/features/render/BeeHiveHelper.java
@@ -5,14 +5,13 @@ import net.minecraft.block.entity.BeehiveBlockEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 import static net.The2019.NewBase.config.ModuleConfig.readModule;
 import static net.The2019.NewBase.config.ModuleStates.beehiveRender;
-import static net.The2019.NewBase.render.WorldRender.renderBox;
+import static net.The2019.NewBase.render.WorldRender.drawBox;
 import static net.The2019.NewBase.utils.ChunkStream.getBlockEntities;
 
 public class BeeHiveHelper {
@@ -27,7 +26,7 @@ public class BeeHiveHelper {
                     for (BlockEntity blockEntity : getBlockEntities().collect(Collectors.toCollection(ArrayList::new))) {
                         if (blockEntity instanceof BeehiveBlockEntity) {
                             BlockPos blockEntityPos = blockEntity.getPos();
-                            renderBox(context, new Box(blockEntityPos));
+                            drawBox(context.matrixStack(), context.consumers(), blockEntityPos, blockEntityPos, 1.0f, 1.0f, 1.0f, 1.0f);
                         }
                     }
                 }
--- src/client/java/net/The2019/NewBase/render/WorldRender.java
+++ src/client/java/net/The2019/NewBase/render/WorldRender.java
@@ -124,4 +124,22 @@ public class WorldRender {
             matrices.pop();
         }
     }
+
+    public static void drawBox(MatrixStack matrices, VertexConsumerProvider vertexConsumers, BlockPos pos1, BlockPos pos2, float red, float green, float blue, float alpha) {
+        Camera camera = MinecraftClient.getInstance().gameRenderer.getCamera();
+        if (camera.isReady()) {
+            Vec3d vec3d = camera.getPos().negate();
+            Box box = Box.enclosing(pos1, pos2).offset(vec3d);
+            drawBox(matrices, vertexConsumers, box, red, green, blue, alpha);
+        }
+    }
+
+    public static void drawBox(MatrixStack matrices, VertexConsumerProvider vertexConsumers, Box box, float red, float green, float blue, float alpha) {
+        drawBox(matrices, vertexConsumers, box.minX, box.minY, box.minZ, box.maxX, box.maxY, box.maxZ, red, green, blue, alpha);
+    }
+
+    public static void drawBox(MatrixStack matrices, VertexConsumerProvider vertexConsumers, double minX, double minY, double minZ, double maxX, double maxY, double maxZ, float red, float green, float blue, float alpha) {
+        VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getDebugFilledBox());
+        WorldRenderer.renderFilledBox(matrices, vertexConsumer, minX, minY, minZ, maxX, maxY, maxZ, red, green, blue, alpha);
+    }
 }
